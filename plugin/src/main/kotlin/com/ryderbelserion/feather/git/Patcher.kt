98629ff50530d798b5dc43464d86d794bf39e9b8
Ryder Belserion <no-reply@ryderbelserion.com>
2023-04-28 00:07:52 -0400

    Update Patcher.kt

--- plugin/src/main/kotlin/com/ryderbelserion/feather/git/Patcher.kt
+++ plugin/src/main/kotlin/com/ryderbelserion/feather/git/Patcher.kt
@@ -5,26 +5,6 @@ import kotlin.system.exitProcess
 
 class Patcher {
 
-    fun gitHistory(startHash: String, endHash: String): String {
-        return shellRun("git", listOf("log", "$startHash..$endHash", "--format=format:%h %s"))
-    }
-
-    fun gitHistory(startHash: String, endHash: String, user: String, project: String, newLine: String): String {
-        val commit = shellRun("git", listOf("log", "$startHash..$endHash", "--format=format:%h %s"))
-
-        val hash = commit.take(7)
-        val msg = commit.substring(8)
-
-        return "[$hash](https://github.com/$user/$project/commit/$hash) $msg$newLine"
-    }
-
-    fun gitFormat(value: String, user: String, project: String, newLine: String): String {
-        val hash = value.take(7)
-        val msg = value.substring(8)
-
-        return "[$hash](https://github.com/$user/$project/commit/$hash) $msg$newLine"
-    }
-
     fun gitExists() {
         runCatching {
             shellRun("git", listOf("--version"))
